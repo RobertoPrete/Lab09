aec2d6fc9fe3f23e45501a3cb84487dce8d9d071
FranPi14 <123479714+FranPi14@users.noreply.github.com>
2025-05-05 16:09:53 +0200

    add soluzione

--- UI/controller.py
+++ UI/controller.py
@@ -8,10 +8,24 @@ class Controller:
         # the model, which implements the logic of the program and holds the data
         self._model = model
 
-    def handle_hello(self, e):
-        name = self._view.txt_name.value
-        if name is None or name == "":
-            self._view.create_alert("Inserire il nome")
+    def handleAnalizza(self,e):
+        threshold = self._view._txtIn.value
+        try:
+            float(threshold)
+        except ValueError:
+            self._view._txt_result.controls.clear()
+            self._view._txt_result.controls.append(ft.Text("Please provide a numerical value for distance. "))
+            self._view.update_page()
             return
-        self._view.txt_result.controls.append(ft.Text(f"Hello, {name}!"))
+
+        self._model.buildGraph(float(threshold))
+
+        self._view._txt_result.controls.clear()
+        self._view._txt_result.controls.append(ft.Text("Graph created!"))
+        self._view._txt_result.controls.append(ft.Text("Num of nodes:" + str(self._model.getNumNodes())))
+        self._view._txt_result.controls.append(ft.Text("Num of edges:" + str(self._model.getNumEdges())))
+        allEdges = self._model.getAllEdges()
+        for edge in allEdges:
+            self._view._txt_result.controls.append(ft.Text(f"{edge[0]}->{edge[1]} -- AvgDist: {self._model.getAvgDist(edge[0],edge[1])}"))
+
         self._view.update_page()
